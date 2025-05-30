df8ccee6925abd63bb161ead055d1528ead12394
Mara <marina.kazsntseva.2005@gmail.com>
2025-05-30 19:06:07 +0700

    добавлен Groupnav Header завершён

--- src/components/Header/GroupNav/GroupNav.jsx
+++ src/components/Header/GroupNav/GroupNav.jsx
@@ -4,7 +4,7 @@ import s from './GroupNav.module.css'
 const GroupNav = () => {
     return(
         <div>
-
+            <p>Мужские группы</p>
         </div>
     );
 };
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,12 @@
 import MainNav from './MainNav/MainNav'
 import s from './Header.module.css'
-
+import GroupNav from './GroupNav/GroupNav'
 
 const Header = () => {
     return(
         <div className={s.header}>
             <MainNav />
+            <GroupNav />
         </div>
     )
 };
